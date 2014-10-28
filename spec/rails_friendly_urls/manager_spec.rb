eb5004bbffd4384148e1979cac627833b418af3d
Carlos Alonso <mrcalonsoperez@gmail.com>
2014-10-28 21:39:10 +0000

    added route injection specs

--- spec/rails_friendly_urls/manager_spec.rb
+++ spec/rails_friendly_urls/manager_spec.rb
@@ -1,40 +1,56 @@
 
 require 'spec_helper'
 
+shared_examples 'a successfully injected friendly url' do
+  let(:route_set) { Rails.application.routes }
+
+  before do
+    subject.urls = [url]
+    subject.inject_urls ActionDispatch::Routing::Mapper.new route_set
+  end
+
+  it 'successfully injects the url' do
+    route_info = Rails.application.routes.recognize_path url.slug
+    route_info.delete(:controller).should == url.controller
+    route_info.delete(:action).should == url.action
+    route_info.should == url.defaults
+  end
+
+  it 'adds the corresponding redirection route' do
+    route_info = Rails.application.routes.recognize_path url.path
+    route_info[:status].should == 301
+    route_info[:path].should == url.slug
+  end
+
+  it 'keeps path helpers working without changing code' do
+    route_set.url_helpers.a_b_c_path.should == url.slug
+  end
+
+  after do
+    route_set.clear!
+  end
+end
+
 describe RailsFriendlyUrls::Manager do
 
   describe '#inject_urls' do
 
     subject { RailsFriendlyUrls::Manager }
 
-    let(:url) {
-      DummyFriendlyURL.new '/a/b/c', '/friendly1', 'application', 'acti', a:1, b:2
-    }
+    context 'with params in the url' do
+      let(:url) {
+        DummyFriendlyURL.new '/a/b/c', '/friendly1', 'application', 'acti', a:1, b:2
+      }
 
-    #TODO test with no params
-
-    let(:route_set) { Rails.application.routes }
-
-    before do
-      subject.urls = [url]
-      subject.inject_urls ActionDispatch::Routing::Mapper.new route_set
+      include_examples 'a successfully injected friendly url'
     end
 
-    it 'successfully injects the url' do
-      route_info = Rails.application.routes.recognize_path url.slug
-      route_info.delete(:controller).should == url.controller
-      route_info.delete(:action).should == url.action
-      route_info.should == url.defaults
-    end
+    context 'with no params in the url' do
+      let(:url) {
+        DummyFriendlyURL.new '/a/b/c', '/friendly1', 'application', 'acti', {}
+      }
 
-    it 'adds the corresponding redirection route' do
-      route_info = Rails.application.routes.recognize_path url.path
-      route_info[:status].should == 301
-      route_info[:path].should == url.slug
-    end
-
-    it 'keeps path helpers working without changing code' do
-      route_set.url_helpers.a_b_c_path.should == url.slug
+      include_examples 'a successfully injected friendly url'
     end
   end
 end
